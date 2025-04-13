acefd6c1013935156c677e584f44c844de4fb261
sekyeong <sekoonglee@gmail.com>
2025-04-14 02:03:57 +0900

    chore: 서버 통신을 위한 API URL 상수를 crawling constants에 추가

--- src/constants/crawling.js
+++ src/constants/crawling.js
@@ -2,3 +2,4 @@ export const SCROLL_ITERATIONS = 50;
 export const SCROLL_DELAY_MS = 300;
 export const IMAGE_LIMIT = 20;
 export const JSON_INDENT = 2;
+export const SERVER_API_URL = "http://localhost:8000/api/v1/images";
