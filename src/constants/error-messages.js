49e3494d3392e86f0bea2d9a248d5aa7781e952b
sekyeong <sekoonglee@gmail.com>
2025-08-23 12:21:33 +0900

    refactor: error-messages상수파일을 typescript로 마이그레이션

--- src/constants/error-messages.js
+++
@@ -1,1 +0,0 @@
-export const ERROR_UNSUPPORTED_URL = "지원하지 않는 URL입니다.";
---
+++ src/constants/error-messages.ts
@@ -0,0 +1,3 @@
+export const ERROR_UNSUPPORTED_URL = "지원하지 않는 URL입니다." as const;
+
+export type ErrorMessage = typeof ERROR_UNSUPPORTED_URL;
