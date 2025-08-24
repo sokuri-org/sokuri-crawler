afcc6d6a01c731e11cf8d620f7ec3cf335090321
sekyeong <sekoonglee@gmail.com>
2025-08-24 12:21:33 +0900

    refactor: http-status파일을 typescript로 마이그레이션

--- src/constants/http-status.ts
+++ src/constants/http-status.ts
@@ -1,3 +1,8 @@
 export const HTTP_BAD_REQUEST = 400;
 export const HTTP_NOT_FOUND = 404;
 export const HTTP_INTERNAL_SERVER_ERROR = 500;
+
+export type HttpStatus =
+  | typeof HTTP_BAD_REQUEST
+  | typeof HTTP_NOT_FOUND
+  | typeof HTTP_INTERNAL_SERVER_ERROR;
