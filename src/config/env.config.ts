5699fe8ba2df9306a69d94cd7b27881a54e4457c
sekyeong <sekoonglee@gmail.com>
2025-08-26 14:45:33 +0900

    refactor: env.config파일을 typescript로 마이그레이션

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,7 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 /* eslint-disable no-magic-numbers */
-export const envConfig = {
+
+export interface EnvConfig {
+  PORT: number;
+  SCROLL_DELAY_MS: number;
+  IMAGE_LIMIT: number;
+  JSON_INDENT: number;
+  SERVER_API_URL: string;
+}
+
+export const envConfig: EnvConfig = {
   PORT: Number(process.env.PORT) || 3100,
   SCROLL_DELAY_MS: Number(process.env.SCROLL_DELAY_MS) || 300,
   IMAGE_LIMIT: Number(process.env.IMAGE_LIMIT) || 20,
