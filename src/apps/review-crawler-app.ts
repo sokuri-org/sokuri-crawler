6a25bfdbf202832ceea0eb2e4612135f8f860ea8
sekyeong <sekoonglee@gmail.com>
2025-08-27 13:11:23 +0900

    refactor: review-crawler-app파일을 typescript로 마이그레이션

--- src/apps/review-crawler-app.ts
+++ src/apps/review-crawler-app.ts
@@ -1,14 +1,14 @@
-import express from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import { envConfig } from "../config/env.config.js";
 import reviewCrawlRouter from "../routes/review-crawl.router.js";
 
-const app = express();
+const app: Application = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", reviewCrawlRouter);
 
-const PORT = process.env.PORT || envConfig.PORT;
+const PORT: number = Number(process.env.PORT) || envConfig.PORT;
 app.listen(PORT, () => {
   console.log(`✅ [REVIEW-CRAWLER] 서버 실행 중: http://localhost:${PORT}`);
 });
