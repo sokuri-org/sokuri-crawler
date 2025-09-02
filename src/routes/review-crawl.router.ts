d553fae232c0c5733a559bb63b969b1508cfea14
sekyeong <sekoonglee@gmail.com>
2025-09-02 13:11:33 +0900

    refactor: review-crawl.router 파일을 typescript로 마이그레이션

--- src/routes/review-crawl.router.ts
+++ src/routes/review-crawl.router.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express, { Router } from "express";
 import { handleReviewCrawl } from "../controllers/review-crawl.controller.js";
 
-const router = express.Router();
+const router: Router = express.Router();
 router.post("/bags/images", handleReviewCrawl);
 
 export default router;
